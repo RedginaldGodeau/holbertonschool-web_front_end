2c91c14884f26267dc316553f18fb6df01f0df0f
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2024-08-09 10:40:26 +0200

    update

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage(fullName){
+	alert('Welcome' + fullName);
+}
+
+guillame = welcomeMessage(" "+"Guillaume");
+alex = welcomeMessage(" "+"Alex");
+fred = welcomeMessage(" "+"Fred");
